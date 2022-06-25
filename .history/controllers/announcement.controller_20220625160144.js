dc954b464f1319a11e11529d193d9117cfda155e
ruth22soft <ruthtamiru10@gmail.com>
2022-06-25 19:24:25 +0300

    stage

---
+++ .history/controllers/announcement.controller_20220625160144.js
@@ -0,0 +1,138 @@
+const models = require('../models');
+// const { Server } = require("socket.io");
+
+// const io = new Server({ 
+//     cors: {
+//         origin:""
+//     }
+
+//  });
+
+// io.on("connection", (socket) => {
+//     // ...
+//   });
+  
+//   io.listen(3001); 
+
+function save(req,res) {
+    
+        
+                
+            const announcement = {
+
+                announcement_type: req.body.announcement_type,
+                detail: req.body.detail,
+                file: req.body.file,
+                date: req.body.date,
+                approved: req.body.approved,
+                admin_id: req.body.admin_id
+                
+            }
+         
+            
+            //to create an announcement
+            models.announcement.create(announcement).then(result =>{
+                res.status(201).json({
+                    message: "Announce created successfully",
+                    post: result
+                })  
+            }).catch(error => {
+                res.status(500).json({
+                    message: "Something went wrong while creating an announcement",
+                    error: error
+                });
+            });
+   
+}
+
+//to get a single announcement
+function show(req,res) {
+    const id = req.params.id;
+
+    models.announcement.findByPk(id).then(result => {
+        if(result) {
+            res.status(200).json({
+                message: "announcement found successfully",
+                post: result
+            })
+        }else{
+            res.status(404).json({
+                message: "announcement not found with id " + id
+            })
+        }
+       
+    }).catch(error => {
+        res.status(500).json({
+            message: "Something went wrong while finding an announcement",
+            error: error
+        });
+    })
+}  
+ 
+
+    //to get all Accusations
+    function index(req,res) {
+        models.announcement.findAll().then(result => {
+            res.status(200).json({
+                message: "Announcements found successfully",
+                post: result
+            })
+        }).catch(error => {
+            res.status(500).json({
+                message: "Something went wrong while finding all announcements",
+                error: error
+            });
+        })
+    }
+
+    //to update an announcement
+    function update(req,res) {
+        const id = req.params.id;
+        const updatedUser = {
+
+            announcement_type: req.body.announcement_type,
+            detail: req.body.detail,
+            file: req.body.file,
+            date: req.body.date,
+            approved: req.body.approved,
+            admin_id: req.body.admin_id
+              
+            }
+
+            const announcement_id = req.params.id;
+
+            models.announcement.update(updatedUser, {where: {id: id}}).then(result => {
+                res.status(200).json({
+                    message: "Announcement updated successfully",
+                    post: updatedUser
+                });
+                }).catch(error => {
+                    res.status(500).json({
+                    message: "Something went wrong while updating an announcement",
+                    error: error
+            });
+            })
+        }
+
+        //delete an announcement
+        function destroy(req,res) {
+            const id = req.params.id;
+            models.announcement.destroy({where: {id: id}}).then(result => {
+                res.status(200).json({
+                    message: "announcement deleted successfully"
+                });
+            }).catch(error => {
+                res.status(500).json({
+                    message: "Something went wrong while deleting an announcement",
+                    error: error
+                });
+            })
+        }
+        module.exports = {
+            save: save,
+            show: show,
+            index:index,
+            update: update,
+            destroy: destroy
+        
+        }
---
+++ .history/server_20220625160304.js
@@ -0,0 +1,14 @@
+//const http = require('http');
+const app = require('./app');
+const port = 3000;
+
+const server = http.createServer(app);
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+server.listen(port);
